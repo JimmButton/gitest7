409f7002af3ddd03bdbed7a7046e1e879112ff4d
Robert Borzyszkowski <robbor@gazeta.pl>
2019-11-16 13:50:06 +0100

    dodanie klasy gittest

---
+++ gittest7/src/Gittest.java
@@ -0,0 +1,7 @@
+public class Gittest {
+    public static void main(String[] args) {
+        System.out.println("No to run");
+        System.out.println("Albo nie");
+        System.out.println("Terefere");
+    }
+}
